05d676e5b007e07406ba75bae3e32f52c5d0636c
João Pacheco <joao.pacheco@g.globo>
2023-05-31 08:43:54 -0300

    feat: havoc + noise

--- checker/src/checker.py
+++ checker/src/checker.py
@@ -33,13 +33,19 @@ def noise(nmin: int, nmax: int) -> str:
     n = random.randint(nmin, nmax)
     return "".join(random.choice(noise_alph) for _ in range(n))
 
+def randomPhone():
+    return "".join(random.choice(string.digits) for _ in range(11))
+
 def assert_status_code(logger: LoggerAdapter, path: str, status: str, headers, body, code: int = 200) -> None:
-    status = status[2:-1]
-    status_code = int(status.split(" ")[0])
-    if status_code == code:
-        return
-    logger.error(f"Bad status code during request at {path}: ({status_code} != {code})\n{body}")
-    raise MumbleException(f"Received {status_code} code at {path} failed")
+    try:
+        status = status[2:-1]
+        status_code = int(status.split(" ")[0])
+        if status_code == code:
+            return
+        logger.error(f"Bad status code during request at {path}: ({status_code} != {code})\n{body}")
+        raise MumbleException(f"Received {status_code} code at {path} failed")
+    except:
+        raise MumbleException(f"Error parsing status code")
 
 async def do_login(task: PutflagCheckerTaskMessage, logger: LoggerAdapter, username: str, password: str) -> None:
     path = "/login"
@@ -133,6 +139,64 @@ async def getflag(task: GetflagCheckerTaskMessage, logger: LoggerAdapter, db: Ch
     else:
         logger.debug(f"Data is missing in profile response for team {task.team_name}")
         raise MumbleException("Data is missing in profile response")
+
+@checker.putnoise(0)
+async def putnoise(task: PutnoiseCheckerTaskMessage, logger: LoggerAdapter, db: ChainDB):
+    username = noise(10, 20)
+    password = noise(10, 20)
+    await do_register(task, logger, username, password)
+    token = await do_login(task, logger, username, password)
+    phone = randomPhone()
+    await do_addphone(task, logger, phone, token=token)
+    await db.set("info1", (token, phone))
+
+@checker.getnoise(0)
+async def getnoise(task: GetnoiseCheckerTaskMessage, logger: LoggerAdapter, db: ChainDB):
+    try:
+        token, phone = await db.get("info1")
+    except KeyError:
+        raise MumbleException("Database info1 missing")
+    r = await do_profile(task, logger, token=token)
+    if "data" in r:
+        if len(r["data"]) != 0:
+            if "phones" in r["data"][0]:
+                phones = r["data"][0]["phones"]
+                assert_in(phone, phones, f"Phone missing in profile")
+            else:
+                logger.debug(f"Phones are missing for team {task.team_name}")
+                raise MumbleException("Phones are missing in profile response")
+        else:
+            logger.debug(f"Data is empty in profile response for team {task.team_name}")
+            raise MumbleException("Data is empty in profile response")
+    else:
+        logger.debug(f"Data is missing in profile response for team {task.team_name}")
+        raise MumbleException("Data is missing in profile response")
+
+@checker.havoc(0)
+async def havoc_safado(task: HavocCheckerTaskMessage, logger: LoggerAdapter, db: ChainDB):
+    username = noise(10, 20)
+    password = noise(10, 20)
+    await do_register(task, logger, username, password)
+    token = await do_login(task, logger, username, password)
+    path = f"/user/{username}"
+    status, headers, body = await do_get(task.address, PORT, path, {"x-token":token})
+    assert_status_code(logger, path, status, headers, body, code=403)
+    assert_in("Cant do that", body, "You must protect your /user!")
+
+@checker.havoc(1)
+async def havoc_hacker(task: HavocCheckerTaskMessage, logger: LoggerAdapter, db: ChainDB):
+    path = "/login"
+    username = noise(10, 20)
+    password = noise(10, 20)
+    payload = {"username":username,"password":password}
+    status, headers, body = await do_post(task.address, PORT, path, {}, urlencode(payload, quote_via=quote_plus))
+    assert_status_code(logger, path, status, headers, body, 400)
+
+@checker.havoc(2)
+async def havoc_healthcheck(task: HavocCheckerTaskMessage, logger: LoggerAdapter, db: ChainDB):
+    path = "/"
+    status, headers, body = await do_get(task.address, PORT, path, {})
+    assert_status_code(logger, path, status, headers, body, 200)
 
 
 @checker.exploit(0)
